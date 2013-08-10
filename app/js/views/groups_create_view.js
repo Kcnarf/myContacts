1897f0556bc37f49173dce808b4c1333ad731a42
Kcnarf <fl.franck.lebeau@gmail.com>
2013-08-10 23:03:04 +0200

    define id #create-group-modal for groupsCreateView

--- app/js/views/groups_create_view.js
+++ app/js/views/groups_create_view.js
@@ -1,14 +1,18 @@
 App.GroupsCreateView = Em.View.extend({
 	templateName: 'groups/create',
-	tagName: 'creategroupmodal',
-
+	
 	classNames: ['modal', 'fade', 'in'],
 
 	attributeBindings: ['role', 'aria_hidden:aria-hidden', 'tabindex'],
 	role:"dialog",
 	aria_hidden:"true",
 	tabindex:"-1",
-
+	
+	init: function () {
+		this.elementId = "create-group-modal";
+		this._super();
+	},
+	
 	didInsertElement: function () {
 		return this.$().modal('show');
 	},
