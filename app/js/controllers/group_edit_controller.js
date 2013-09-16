8ccd51eab4f045a0b2404a77ecd63f5c89dd9dc6
Kcnarf <fl.franck.lebeau@gmail.com>
2013-09-16 21:34:08 +0200

    .get('trans').commit() -> .save(), for Groups

--- app/js/controllers/group_edit_controller.js
+++ app/js/controllers/group_edit_controller.js
@@ -1,12 +1,10 @@
 App.GroupEditController = Ember.ObjectController.extend({
   
-	actions: {
-		rollback: function() {
-			this.get('transaction').rollback();
-		},
-		
-		update: function() {
-			this.get('transaction').commit()
-		}
+	rollback: function() {
+		this.get('transaction').rollback();
+	},
+	
+	update: function() {
+		this.get('model').save()
 	}
 })
--- app/js/controllers/groups_create_controller.js
+++ app/js/controllers/groups_create_controller.js
@@ -1,12 +1,10 @@
 App.GroupsCreateController = Ember.ObjectController.extend({
   
-	actions: {
-		rollback: function() {
-			this.get('transaction').rollback();
-		},
-		
-		update: function() {
-			this.get('transaction').commit()
-		}
+	rollback: function() {
+		this.get('transaction').rollback();
+	},
+	
+	update: function() {
+		this.get('model').save()
 	}
 })
